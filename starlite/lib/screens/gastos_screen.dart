60002dbc00463fded6e3dc3abba6075230a11cf8
Alexey Zhelezov <zhelezovalexey@gmail.com>
2020-05-13 13:02:52 +0200

    haciendo cosas

--- starlite/lib/screens/gastos_screen.dart
+++ starlite/lib/screens/gastos_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:starlite/widgets/chart.dart';
 
 class GastosScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final deviceMedia = MediaQuery.of(context);
+    return Column(
+      children: <Widget>[
+        Container(
+          width: double.infinity,
+          height: (deviceMedia.size.height / 2) -
+              Scaffold.of(context).appBarMaxHeight,
+          child: Center(
+            child: Chart(),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black54,
+                blurRadius: 10,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- starlite/lib/screens/home_screen.dart
+++ starlite/lib/screens/home_screen.dart
@@ -5,48 +5,46 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final deviceMedia = MediaQuery.of(context);
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: (deviceMedia.size.height / 2) - Scaffold.of(context).appBarMaxHeight,
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'No tienes ninguna meta aun',
-                  style: TextStyle(fontSize: 20),
-                ),
-                const SizedBox(height: 10),
-                RoundedBlackButton(
-                  child: Text(
-                    'Busca articulos',
-                    style: TextStyle(color: Colors.white, fontSize: 22),
-                  ),
-                  onTap: () {},
-                ),
-              ],
-            ),
-          ),
-          Container(
-            height: (deviceMedia.size.height / 2) - 56,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
+    return Column(
+      children: <Widget>[
+        Container(
+          height: (deviceMedia.size.height / 2) - Scaffold.of(context).appBarMaxHeight,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'No tienes ninguna meta aun',
+                style: TextStyle(fontSize: 20),
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black54,
-                  blurRadius: 10,
+              const SizedBox(height: 10),
+              RoundedBlackButton(
+                child: Text(
+                  'Busca articulos',
+                  style: TextStyle(color: Colors.white, fontSize: 22),
                 ),
-              ],
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: (deviceMedia.size.height / 2) - 56,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
             ),
-          )
-        ],
-      ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black54,
+                blurRadius: 10,
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
